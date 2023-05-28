c6f6d2dddda80759c8373452c9e3bfab72b66fdf
Mahipal Singh <mssodha2510@gmail.com>
2023-05-28 23:16:20 +0530

    okay

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+//const url = 'mongodb+srv://mrdpkydv:Deepak%401314@cluster0.wvxaikj.mongodb.net/employee?retryWrites=true&w=majority';
+const url = process.env.URI;
+// mongoose.connect('url') 
+mongoose.connect(url);
+const db=mongoose.connection;
+
+//If any Error then Getting this Line
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));   
+
+
+db.once('open',()=>{
+    console.log("Connected to Database :: MongoDB ")
+});
+
+module.exports=db;  //Exports db
